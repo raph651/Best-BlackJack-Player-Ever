ba0adf817b43a835892194861b33d22faca76d17
Haoyu Wu <haoyuwu36@gmail.com>
2022-12-02 16:40:19 -0800

    new variant doing well

--- game_env/mcts.py
+++ game_env/mcts.py
@@ -32,18 +32,28 @@ class Node:
         self.parent = parent
         self.prior_action = prior_action
         self.child = child
+        self.network = network
         # count
         self.N = [0]*3
         # total val
         self.W = [0]*3
         # action val
         self.Q = [0]*3
+
         # action prob
         # fill by probability returned by neural net, take only prob
         # add another indexing [0] because the output p has shape [1,3]
         # after adding, it has shape [3]
         if state:
-            self.P = network(torch.FloatTensor([state]))[0][0]
+            self.P = self.network(torch.FloatTensor([state]))[0][0].tolist()
+
+    def copy(self):
+        ans = Node(self.state, self.parent, self.prior_action, self.network, self.child)
+        ans.N = self.N.copy()
+        ans.W = self.W.copy()
+        ans.Q = self.Q.copy()
+        ans.P = self.P.copy()
+        return ans
 
 
 class MCT:
@@ -75,11 +85,16 @@ class MCT:
         return root.child
 
     def get_good_action(self, root):
-        PUCT_alg = lambda action: self.explore_constant * root.P[action] * (sum(root.N))**0.5 / (1 + root.N[action])
+        def PUCT_alg(action):
+            return self.explore_constant * root.P[action] * (sum(root.N))**0.5 / (1 + root.N[action])
 
         def metric(action):
             return PUCT_alg(action) + root.Q[action] # self.network(torch.FloatTensor([root.state]))[1][0].item()
 
+        # print([PUCT_alg(a) for a in self.env.player.actions])
+        # print(root.N)
+        # print(root.P)
+        # input()
         return max(self.env.player.actions, key=metric)
 
     def search(self, root):
@@ -88,22 +103,32 @@ class MCT:
         for _ in range(self.search_amount):
             self.env = deepcopy(temp_env)
             cur = root
-            while True:
-                action = self.get_good_action(cur)
-                # print(tuple(self.env.state.input()), action)
-                reward, done = self.env.step(action)
-                if done:
-                    # symbolic child just for backtrack purpose
-                    cur.child = Node(state=[], parent=cur, prior_action=action, network=None)
-                    cur = cur.child
-                    # print("reward: ", reward)
-                    self.backtrack(cur, reward, root, train=0)
-                    break
+            # perform search on each action before choosing the best
+            action = self.search_then_refine(cur, _, thre=0.15)
+            reward, done = self.env.step(action)
+            while not done:
+                # choose the best action on already explored node
                 cur = self.get_child(cur, tuple(self.env.state.input()), action)
-            # input("end run")
+                action = self.get_good_action(cur)
+                reward, done= self.env.step(action)
+
+            # symbolic child just for backtrack purpose
+            cur.child = Node(state=[], parent=cur, prior_action=action, network=None)
+            cur = cur.child
+            self.backtrack(cur, reward, root, train=0)
 
         self.env = temp_env
         self.dataset.add({'state':root.state,'Q':root.Q,'P':root.P})
+
+    def search_then_refine(self, cur, _, thre):
+        if _<self.search_amount*thre/3:
+            return 0
+        elif _<self.search_amount*thre*2/3:
+            return 1
+        elif _<self.search_amount*thre:
+            return 2
+        else:
+            return self.get_good_action(cur)
 
     def run(self, root):
         if root.state and root.state not in self.states:
@@ -112,7 +137,8 @@ class MCT:
         cur = root
         while True:
             action = self.get_good_action(cur)
-            print(self.env.state.input(), action)
+            print(self.env.state.input(), action, " prob_net: ",
+                  self.network(torch.FloatTensor([cur.state]))[0][0].tolist(), " prob_real: ", root.P, " q: ", root.Q)
             reward, done = self.env.step(action)
             if done:
                 self.backtrack(cur, reward, root, train=1)
@@ -122,17 +148,16 @@ class MCT:
     def backtrack(self, cur, reward, root, train):
         while cur.state != root.state:
             parent = cur.parent
+            # print("state: ", parent.state, " action: ", cur.prior_action)
             prior_action = cur.prior_action
             parent.W[prior_action] += reward
             parent.N[prior_action] += 1
             parent.Q[prior_action] = parent.W[prior_action]/parent.N[prior_action]
             parent.P = [n/sum(parent.N) for n in parent.N]
+            cur = parent
 
-            cur = cur.parent
             if train:
                 self.dataset.add({'state':cur.state,'Q':cur.Q,'P':cur.P})
-            if cur.state != root.state:
-                break
 
     def train(self):
         loss_list = []
@@ -174,11 +199,11 @@ def plot(reward, mv_reward, losses):
 
 def simulate(new_model, training_itr, deck_num, search_amount, explore_constant):
     def criterion(p, v, q, pi):
-        loss = (v-torch.sum(q*pi, dim=-1).unsqueeze(-1))**2 - torch.sum(pi * torch.log(p+0.01), dim=-1).unsqueeze(-1)
+        loss = (v-torch.sum(q*pi, dim=-1).unsqueeze(-1))**2 - torch.sum(pi * torch.log(p+0.0001), dim=-1).unsqueeze(-1)
         return torch.sum(loss)
 
     network = qnet.QNet()
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.01, weight_decay=0.01)
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.001, weight_decay=0.0001)
     tree = MCT(network, optimizer, criterion)
     tree.reset(deck_num, search_amount, explore_constant)
 
@@ -210,8 +235,7 @@ def simulate(new_model, training_itr, deck_num, search_amount, explore_constant)
             loss_list = tree.train()
             losses.extend(loss_list)
     plot(rewards, mv_rewards, losses)
-            # print(network.state_dict())
-
+    print(sum(rewards)/len(rewards))
 
     torch.save({
         'model_state_dict': network.state_dict(),
@@ -220,5 +244,38 @@ def simulate(new_model, training_itr, deck_num, search_amount, explore_constant)
     with open('data.pkl', 'wb') as file:
         pickle.dump((tree.states, tree.dataset), file)
 
+def test(test_itr, deck_num, search_amount, explore_constant):
+    network = qnet.QNet()
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.001, weight_decay=0.001)
+    tree = MCT(network, optimizer, criterion=None)
+    tree.reset(deck_num, search_amount, explore_constant)
+
+    PATH = "param.pth"
+    checkpoint = torch.load(PATH)
+    network.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    with open('data.pkl', 'rb') as file:
+        tree.states, tree.dataset = pickle.load(file)
+
+    rewards = []
+    for _ in range(test_itr):
+        root = Node(state=tree.env.state.input(), parent=None, prior_action=None, network=tree.network)
+        while True:
+            prob_network, _v = tree.network(torch.FloatTensor([root.state]))
+            prob_network = prob_network.tolist()
+            print("state: ", root.state, "action_prob: ", prob_network)
+            input()
+            action = prob_network.index(max(prob_network))
+            reward, done = tree.env.step(action)
+            if done:
+                rewards.append(reward)
+                break
+            root.child = Node(state=tree.env.state.input(), parent=root, prior_action=action, network=tree.network)
+            root = root.child
+    plt.plot(rewards)
+    plt.show()
+    print(sum(rewards)/len(rewards))
+
 if __name__ == '__main__':
-    simulate(new_model=1, training_itr=1000, deck_num=6, search_amount=300, explore_constant=0.1)
+    simulate(new_model=1, training_itr=100, deck_num=6, search_amount=300, explore_constant=100000)
+    # test(test_itr=100, deck_num=6, search_amount=None, explore_constant=None)
