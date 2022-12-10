01582fc2f217b7b9f8335493b4c2283730f7131c
raph651 <raph651@uw.edu>
2022-12-10 15:44:25 -0800

    new -60 model with biggernet

---
+++ game_env/biggernet.py
@@ -0,0 +1,54 @@
+from torch import nn
+
+class QNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(16,96),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(96,256),
+            nn.LeakyReLU(inplace=True),
+            nn.Dropout(),
+
+            nn.Linear(256,512),
+            nn.LeakyReLU(inplace=True),
+            nn.Dropout(),
+
+            nn.Linear(512,512),
+            nn.LeakyReLU(inplace=True)
+        )
+        self.prob_decoder = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512,256),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(),
+            nn.Linear(256,128),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(),
+            nn.Linear(128,64),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(64,3),
+            nn.Softmax(dim=1),
+        )
+        self.value_decoder = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512,256),
+            nn.ReLU(inplace=True),
+            
+            nn.Dropout(),
+            nn.Linear(256,128),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(),
+            nn.Linear(128,24),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(24,1),
+            nn.Softsign(),
+        )
+    def forward(self,x):
+        shared = self.encoder(x)
+        return self.prob_decoder(shared), self.value_decoder(shared)
